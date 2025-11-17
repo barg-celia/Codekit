1cc371c14ae2c7e390e3dc637ec121e2e0fdc8cb
barg-celia <bargoincelia@gmail.com>
2025-11-17 09:19:28 +0100

    burger menu
    partie 2

--- BurgerMenu/assets/script.js
+++ BurgerMenu/assets/script.js
@@ -2,13 +2,18 @@
 
 const menuToggle = document.querySelector(".menu__toggle");
 const menu = document.querySelector(".menu");
+const menuLinks = document.querySelectorAll(".menu__el a");
 
 if(menuToggle){
+    // click sur le bouton
     menuToggle.addEventListener("click", menuOpen);
-}
 
+    // click sur les liens 
+    for(let menuLink of menuLinks){
+        menuLink.addEventListener("click", menuOpen);
+    }
+}
+// function pour ouvrir et fermer le menu
 function menuOpen(){
     menu.classList.toggle("menu--open");
 }
-
-// quand on clique sur un des liens le menu se ferme 
